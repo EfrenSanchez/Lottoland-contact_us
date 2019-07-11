2b3edf994858f2156f341bcf2b90bc59215d4922
Efren Sanchez <efren.sanchez@ESanchezMBA.local>
2019-07-11 17:20:00 +0200

    allDevice

--- index.js
+++ index.js
@@ -11,3 +11,36 @@ Evergage.safeClick(".evg__mainCTA", function(e) {
     }
     return isValid;
 })
+
+
+//Prevent Key pad overlapping the phone input (focus => top:0; blur => top:78.5px;)
+// document.getElementById("eg_Phone_number_X5KCD").addEventListener("focus", function(e) {
+    // window.scrollTo(0,document.body.scrollHeight);
+    // document.getElementById("evergage-tooltip-ambpbEOA").style.top = '-100px';
+// });
+
+// document.getElementById("eg_Phone_number_X5KCD").addEventListener("blur", function(e) {
+//     document.getElementById("evergage-tooltip-ambpbEOA").style.top = '78.5px';
+// });
+
+
+const ua = navigator.userAgent.toLowerCase();
+const isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+if(isAndroid) {
+    phoneInput = document.getElementById("eg_Phone_number_X5KCD");
+    modal = document.getElementById("evergage-tooltip-ambpbEOA");
+    clickHere = document.getElementById("clickHereWrapper");
+    
+phoneInput.addEventListener("focus", function(e) {
+    modal.style.overflow = 'scroll';
+    modal.style.height = '300px';
+    clickHere.style.display = 'none';
+});
+
+phoneInput.addEventListener("blur", function(e) {
+    modal.style.overflow = 'initial';
+    modal.style.height = '510px';
+    clickHere.style.display = 'block';
+});
+}
+
